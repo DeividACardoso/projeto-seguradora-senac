6eb3a371a103e916738d897b466392bf7ab91d7a
DEI_WES <DEI_WES@DESKTOP-B3MSG81>
2023-06-14 13:04:41 -0300

    Teste de commit

--- src/main/java/executavel/Executavel.java
+++ src/main/java/executavel/Executavel.java
@@ -11,17 +11,18 @@ import model.vo.TipoSinistro;
 public class Executavel {
 	public static void main(String[] args) {
 		
-		Endereco endereco1 = new Endereco("Rua para teste", "123", "12593578", "Bairro ali", "Florianópolis", "SC");
-		Pessoa pessoa1 = new Pessoa("Deivid Teste", "11144455566", null, 231456978, endereco1);
-		Sinistro sinistro1 = new Sinistro(pessoa1, "123", TipoSinistro.COLISAO, null, null, 1200.0, 1300.0, 2500.0, "Bateu", "pq quis");
-		
-		EnderecoDAO enderecoDAO = new EnderecoDAO();
-		PessoaDAO pessoaDAO = new PessoaDAO();
-		SinistroDAO sinDAO = new SinistroDAO();
-		
-		enderecoDAO.inserir	(endereco1);
-		pessoaDAO.inserir(pessoa1);
-		sinDAO.cadastrarSinistro(sinistro1);
+		System.out.println("batata");
+//		Endereco endereco1 = new Endereco("Rua para teste", "123", "12593578", "Bairro ali", "Florianópolis", "SC");
+//		Pessoa pessoa1 = new Pessoa("Deivid Teste", "11144455566", null, 231456978, endereco1);
+//		Sinistro sinistro1 = new Sinistro(pessoa1, "123", TipoSinistro.COLISAO, null, null, 1200.0, 1300.0, 2500.0, "Bateu", "pq quis");
+//		
+//		EnderecoDAO enderecoDAO = new EnderecoDAO();
+//		PessoaDAO pessoaDAO = new PessoaDAO();
+//		SinistroDAO sinDAO = new SinistroDAO();
+//		
+//		enderecoDAO.inserir	(endereco1);
+//		pessoaDAO.inserir(pessoa1);
+//		sinDAO.cadastrarSinistro(sinistro1);
 	}
 		
 	
